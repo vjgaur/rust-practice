dd2c131eace7ec6e0e80ac998c91e9ae8487ce6c
vjgaur <vijayendra.gaur@gmail.com>
2022-11-04 20:42:52 +0530

    match with arbitrary identifier

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
-}
+
+    fn main() {
+        let name = String::from("Alice");
+        let num = 100;
+
+        match name.as_str() {
+            "Bob" => println!("I am Bob"),
+            "Alice" => println!("I am Alice"),
+              _ => println!("Default"),
+        }
+        match num {
+            n => println!("I am Alice"),
+            100 => println!("I am Bob"),
+        }
+    }
+    
+
